6781e2e96d555946634f0604c72f6ef0936fb5aa
Jodie Drover <jodiedrover@live.com>
2023-10-23 16:15:44 -0400

    Add long review comment for styling

--- db/seeds/05_reviews.sql
+++ db/seeds/05_reviews.sql
@@ -2,3 +2,4 @@ insert into reviews (user_id, user_image, rating, comment, created_at, updated_a
 insert into reviews (user_id, user_image, rating, comment, created_at, updated_at) values ('user-2', NULL, 5, 'Excellent service!', '2023-09-26 12:30:00', NULL);
 insert into reviews (user_id, user_image, rating, comment, created_at, updated_at) values ('user-3', NULL, 5, 'Loved the Pho!', '2022-08-01 10:00:14', NULL);
 insert into reviews (user_id, user_image, rating, comment, created_at, updated_at) values ('user-4', NULL, 3, 'Pretty good.', '2023-02-09 23:51:53', NULL);
+insert into reviews (user_id, user_image, rating, comment, created_at, updated_at) values ('user-5', NULL, 5, 'Now we have to see if something that is pretty long super duper long the longest comment we can write! its gonna be all kinds of messed up ', '2023-02-09 23:51:53', NULL);
