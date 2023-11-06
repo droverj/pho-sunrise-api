538bb6a6d3bbccd46774f1c6eb22553e754a6f74
Jodie Drover <jodiedrover@live.com>
2023-11-06 15:10:02 -0500

    Rough draft for items db

--- db/schema/01_items.sql
+++ db/schema/01_items.sql
@@ -4,8 +4,8 @@ CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   section VARCHAR(50) NOT NULL,
   section_vietnamese VARCHAR(50) NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  name_vietnamese VARCHAR(50) NOT NULL,
+  name VARCHAR(150) NOT NULL,
+  name_vietnamese VARCHAR(150) NOT NULL,
   item_option VARCHAR(50),
   price NUMERIC(10, 2) NOT NULL
 );
