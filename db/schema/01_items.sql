cf758027c1991f19099b2d1c09027dc0bd555897
Jodie Drover <jodiedrover@live.com>
2023-11-06 17:20:52 -0500

    Update items

--- db/schema/01_items.sql
+++ db/schema/01_items.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS items CASCADE;
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   section VARCHAR(50) NOT NULL,
-  section_vietnamese VARCHAR(50) NOT NULL,
+  section_vietnamese VARCHAR(50),
   name VARCHAR(150) NOT NULL,
-  name_vietnamese VARCHAR(150) NOT NULL,
+  name_vietnamese VARCHAR(150),
   item_option VARCHAR(50),
   price NUMERIC(10, 2) NOT NULL
 );
